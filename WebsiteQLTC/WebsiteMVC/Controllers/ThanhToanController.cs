dd727c85fa4f4a400df8b301d7770211cc1f4b3c
huyqa170395 <huyqa170395@tvu-onschool.edu.vn>
2024-12-17 13:34:47 +0700

    HuyQA-First-Commit

---
+++ WebsiteQLTC/WebsiteMVC/Controllers/ThanhToanController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteMVC.Models;
+
+namespace WebsiteMVC.Controllers
+{
+    public class ThanhToanController : BaseController
+    {
+        private QLTCEntities db = new QLTCEntities();
+
+        // GET: ThanhToan
+        public ActionResult Index(int id)
+        {
+            var thanhToans = db.ThanhToans.Where(q => q.IDCongNo == id);
+            ViewBag.IDCongNo = id;
+            ViewBag.congno = db.CongNoes.Find(id);
+            return View(thanhToans.ToList());
+        }
+
+        // GET: ThanhToan/Edit/5
+        public ActionResult Edit(int? id, int? IDCongNo)
+        {
+            ThanhToan thanhToan = null;
+            if (id.HasValue)
+            {
+                thanhToan = db.ThanhToans.Find(id);
+            }
+            else
+            {
+                thanhToan = new ThanhToan()
+                {
+                    IDCongNo = IDCongNo,
+                    CongNo = db.CongNoes.Find(IDCongNo)
+                };
+            }
+            if (thanhToan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thanhToan);
+        }
+
+        // POST: ThanhToan/Edit/5
+        [HttpPost]
+        public ActionResult Edit(ThanhToan thanhToan)
+        {
+            var congno = db.CongNoes.Find(thanhToan.IDCongNo);
+            if (thanhToan.IDThanhToan > 0)
+            {
+                db.Entry(thanhToan).State = EntityState.Modified;
+            }
+            else
+            {
+                thanhToan.CreateTime = DateTime.Now;
+                thanhToan.CreateBy = Account.IDTaiKhoan;
+                thanhToan.State = Notify.INIT;
+                congno.Payed += thanhToan.SoTienTra;
+                db.ThanhToans.Add(thanhToan);
+            }
+            if (congno.Payed >= congno.PhaiTra)
+            {
+                congno.State = Notify.SUCCESS;
+            }
+            congno.ConNo = congno.Tra1Ngay * (congno.NgayTra - thanhToan.CreateTime).Value.Days - congno.Payed;
+            db.SaveChanges();
+            return RedirectToAction("Index", "CongNo", new { thanhToan.IDCongNo });
+        }
+
+        public JsonResult Delete(int id)
+        {
+            ThanhToan thanhToan = db.ThanhToans.Find(id);
+            db.ThanhToans.Remove(thanhToan);
+            return Json(db.SaveChanges());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public JsonResult State(int id, byte state)
+        {
+            db.ThanhToans.Find(id).State = state;
+            return Json(db.SaveChanges() > 0);
+        }
+    }
+}
---
+++ WebsiteQLTC/WebsiteMVC/Models/DTOExtention.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteMVC.Models
+{
+    partial class Quy
+    {
+        public string mName { get => $"{MaQuy} {(Money ?? 0).ToString("#,###")}VND"; }
+    }
+
+    partial class CongNo
+    {
+        public bool Cong { get; set; }
+        public decimal? TyLe => Payed * 100 / (Payed + ConNo);
+        public decimal? Tra1Ngay => (int)((SoTien / (NgayTra - CreateTime).Value.Days) * (100 + (decimal)LaiSuat) / 100);
+        public decimal? PhaiTra => Payed + (NgayTra - DateTime.Now).Value.Days * Tra1Ngay;
+    }
+
+    public partial class ThanhToan
+    {
+        public int? Timed => (DateTime.Now - CongNo.CreateTime)?.Days;
+    }
+
+    partial class ThuChi
+    {
+        public bool Thu { get; set; }
+    }
+
+}
